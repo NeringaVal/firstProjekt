11836601d527bafdc0ee4953a3b5f2d582f1eb30
neringaVal <neringutev@gmail.com>
2022-11-18 13:07:13 +0200

    Pakitimas1

--- index.php
+++ index.php
@@ -8,5 +8,45 @@
 </head>
 <body>
     <h1>hi</h1>
+    <?php
+    $firstName = "Neringa";
+    $lastName = "Valeikienė";
+    $data = 1970;
+    $nowYear = 2022;
+    $Years = $nowYear - $data;
+    echo "<p> Aš esu  $firstName  $lastName. Man yra $Years metų </p>";
+    echo "Aš esu ". $firstName ." ". $lastName. ". Man yra " . $Years . " metų"."<br>"; 
+
+    $firstNamber = rand(0, 4);
+    $secondNamber = rand(0, 4);
+    if ($firstNamber > $secondNamber) {
+        if ($secondNamber != 0) {
+            $dalmuo = $firstNamber / $secondNamber;
+            echo round($dalmuo , 2);
+        } else{
+            echo "<p> Dalyba negalima</p>";
+        } 
+    }
+    else  {
+        if ($firstNamber != 0) {
+            $dalmuo = $secondNamber / $firstNamber;
+            
+            echo round($dalmuo , 2);
+        } else {
+            echo "<p> Dalyba negalima</p>";
+        }
+    }
+    
+    $a = rand(1, 10);
+    $b = rand(1, 10);
+    $c = rand(1, 10);
+    if ($a < $b + $c && $b < $a + $c && $c < $a + $b) {
+        echo "<p> Galima</p>";
+    }
+    else  {
+        echo "<p> negalima</p>";
+    }
+ 
+?>
 </body>
 </html>
